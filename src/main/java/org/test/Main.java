7f72c4d481656c0a72d774e216020f9cc645ee5c
Liuker <liukersun@gmail.com>
2023-06-07 16:47:26 +0800

    rm: Remove uselsee dependencies.

--- src/main/java/org/test/Main.java
+++ src/main/java/org/test/Main.java
@@ -14,6 +14,7 @@ public class Main {
     public static final org.apache.logging.log4j.Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) throws Exception {
+        logger.info("Start");
         // load config from Json File.
         JsonNode configJson = loadJson("Data/config.json");
         // get tempFilePath and resultFilePath.
